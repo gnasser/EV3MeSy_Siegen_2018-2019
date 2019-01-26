fc7586dd19ddd03d6b269776e8e99e9cebde8793
Samira Sánchez <samirasancheze@gmail.com>
2019-01-26 13:59:16 -0400

    Codigo final
    - sigue la línea con poco cabeceo hacia los lados.
    
    - se desplaza de estacion inicial a carga
    
    - toma caja en estacion de carga.
    
    - Se desplaza a estacion de almacen
    
    - Deja caja en almacen.
    
    - Vuelve a inicio y repite rutina.

--- 18_01_22_Control_general_class.py
+++ 18_01_22_Control_general_class.py
@@ -4,7 +4,8 @@
 #Se esta reeatructurando el c'odigo para que acepte distintos sensores
 ##Lee sensores de proximidad y color, tiene topico para publicar a motor que mueve el brazo
 # se corrige la trayectoria solo girando en torno al centro de masa del robot si se desvia un poco del centro de la linea
-# Al encontrar un obstaculo a 12cm se detiene, avanza un poco y luego retrocede la misma distancia
+# Al encontrar un obstaculo a 12cm se detiene, baja brazo, avanza un poco, sube brazo y luego gira a la izquierda para seguir el algoritmo de linea
+# Al llegar al almacen se detiene, baja brazo, retrocede, sube brazo, gira a la izquierda para seguir el algoritmo de seguir linea.
 
 import rospy
 from sensor_msgs.msg import Illuminance	# color Sensor library
@@ -13,8 +14,8 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 import time
 
-motor_v_iz=0	# velocidad de motor
-motor_v_der=0	# velocidad de motor
+motor_v_L=0	# velocidad de motor
+motor_v_R=0	# velocidad de motor
 inicio=0
 
 #Se agrega el Publisher---------------------------------
@@ -24,78 +25,109 @@ inicio=0
 class sensor_data:	# En esta clase se almacenan los datos recibidos por los sensores
 	def __init__(self):	#Creacion de la clase	
 		self.il=0.0		#En il se almacena el valor de la iluminancia recibida por el sensor de color
-		self.dist=0.0
+		self.dist=0.5
+		self.caja=0
 	def iluminacion (self,data):
 		self.il=data.illuminance # Recibido un valor de iluminacion, actualizacion del dato. illuminance number 0 (dark) to 100 (bright)
 		return self.il
 	def distancia(self,data):
 		self.dist=data.range
 		return self.dist
-
-
-
+#--------------------------------------------------------
 def motors_velocity(il,dist):
-	motor_base=1
-	
-	if il < 33 and il > 13 :	# robot sigue derecho
-		motor_v_iz=motor_base
-		motor_v_der=motor_base
-	
-	elif il > 34:	# robot desvia a derecha
+	motor_base=1.4
+	if il < 30 and il > 13 :	# robot sigue derecho
+		motor_v_L=motor_base
+		motor_v_R=motor_base
+	elif il >= 30:	# robot desvia a derecha
 		print('blanco')
-		motor_v_iz=-1
-		motor_v_der= 1		
+		motor_v_L=-1
+		motor_v_R= 1		
 	else:		#robot desvia a izquierda
 		print('negro')
-		motor_v_iz=1#motor_base+(correccion)
-		motor_v_der=-1#-motor_base+(correccion)
+		motor_v_L=1#motor_base+(correccion)
+		motor_v_R=-1#-motor_base+(correccion)
 	if il<3:
-		motor_v_iz=0
-		motor_v_der=0		
-	return motor_v_iz, motor_v_der
+		motor_v_L=0
+		motor_v_R=0		
+	return motor_v_L, motor_v_R
 #-------------------------------------------------------
 def listener_talker():
 	inicio=0	#variable que indica si se leyeron los valores de negro y blanco antes de iniciar los motores
 	SensorData=sensor_data()
+	delay_palanca=3.4
 #--------Creacion de los nodos a utilizar----------
 	rospy.init_node('listener_talker_color', anonymous=True)	# Creacion del nodo que se suscribe al sensor de color
-	pub=rospy.Publisher('/ev3dev/OutPortA/command', Float64,queue_size=10)	# the node is publishing to the /ev3dev/OutPortA/command topic, motorA velocity
-	pub2=rospy.Publisher('/ev3dev/OutPortB/command', Float64,queue_size=10)	# the node is publishing to the /ev3dev/OutPortB/command topic, motorB velocity
-	pub3=rospy.Publisher('/ev3dev/OutPortC/command', Float64,queue_size=10)	# the node is publishing to the /ev3dev/OutPortC/command topic, motorC velocity
+	pub_motor_L=rospy.Publisher('/ev3dev/OutPortA/command', Float64,queue_size=10)	# the node is publishing to the /ev3dev/OutPortA/command topic, motorL velocity
+	pub_motor_R=rospy.Publisher('/ev3dev/OutPortB/command', Float64,queue_size=10)	# the node is publishing to the /ev3dev/OutPortB/command topic, motorR velocity
+	pub_motor_C=rospy.Publisher('/ev3dev/OutPortC/command', Float64,queue_size=10)	# the node is publishing to the /ev3dev/OutPortC/command topic, motorC velocity
 	rospy.Subscriber('color', Illuminance, SensorData.iluminacion)	#To read the color from sensor, the node is subscribed to /color topic
 	rospy.Subscriber('ultrasonic', Range, SensorData.distancia)
 	rate=rospy.Rate(10)
 #Publicar-----------------------------------------------
 	while not rospy.is_shutdown():
-		'''if inicio==0:	#es necesario calibrar
-			white, black, midpos = calibracion(SensorData.il)
-			inicio=1	#Ya se calibro el sistema'''
 		distancia_m=SensorData.dist
 		print "distancia" , distancia_m
 		rospy.loginfo(distancia_m)
-		if distancia_m <= 0.10:# and distancia_m <= 0.125:	#the robot detect a box
-			motor_v_iz = 0
-			motor_v_der = 0
-		 	rospy.loginfo(motor_v_iz)
-	 		rospy.loginfo(motor_v_der)
-			pub.publish(Float64(motor_v_iz))	# The velocity of motor A
-		 	pub2.publish(Float64(motor_v_der))	# The velocity of motor B
-			rospy.sleep(2)
-			pub.publish(Float64(1))	# The velocity of motor A
-		 	pub2.publish(Float64(1))	# The velocity of motor B
-			rospy.sleep(1.8)
-			pub.publish(Float64(0))	# The velocity of motor A
-		 	pub2.publish(Float64(0))	# The velocity of motor B'''
-			rospy.sleep(2)
-			pub.publish(Float64(-1))	# The velocity of motor A
-		 	pub2.publish(Float64(-1))	# The velocity of motor B
-			rospy.sleep(2)
+		if distancia_m <= 0.11:# and distancia_m <= 0.125:	#the robot detect a box
+			if SensorData.caja==0:
+				pub_motor_L.publish(Float64(0))		# The velocity of motor A
+			 	pub_motor_R.publish(Float64(0))	# The velocity of motor B
+				rospy.sleep(.5)
+				# baja palanca
+				pub_motor_C.publish(Float64(-1))	# The velocity of motor B
+				rospy.sleep(delay_palanca)
+				pub_motor_C.publish(Float64(0))	# The velocity of motor B
+				rospy.sleep(1)
+				# Avanza hacia adelante
+				pub_motor_R.publish(Float64(1))	# The velocity of motor B
+				pub_motor_L.publish(Float64(1))		# The velocity of motor A
+				rospy.sleep(2)
+				pub_motor_L.publish(Float64(0))		# The velocity of motor A
+			 	pub_motor_R.publish(Float64(0))	# The velocity of motor B
+				rospy.sleep(1)
+				# sube la palanca
+				pub_motor_C.publish(Float64(1))	# The velocity of motor C
+				rospy.sleep(delay_palanca)
+				pub_motor_C.publish(Float64(0))	# The velocity of motor C
+				rospy.sleep(1)
+				# gira hacia izquierda
+				pub_motor_L.publish(Float64(-1.4))	# The velocity of motor A
+			 	pub_motor_R.publish(Float64( 1.4))	# The velocity of motor B
+				rospy.sleep(2)
+				SensorData.caja=1
+			else:
+				pub_motor_L.publish(Float64(0))		# The velocity of motor A
+			 	pub_motor_R.publish(Float64(0))		# The velocity of motor B
+				rospy.sleep(.5)
+				# baja palanca
+				pub_motor_C.publish(Float64(-1))	# The velocity of motor B
+				rospy.sleep(delay_palanca)
+				pub_motor_C.publish(Float64(0))	# The velocity of motor B
+				rospy.sleep(1)
+				# retrocede
+				pub_motor_L.publish(Float64(-1))	# The velocity of motor A
+			 	pub_motor_R.publish(Float64(-1))	# The velocity of motor B
+				rospy.sleep(1.7)
+				pub_motor_L.publish(Float64(0))	# The velocity of motor A
+			 	pub_motor_R.publish(Float64(0))	# The velocity of motor B
+				rospy.sleep(0.5)
+				# sube la palanca
+				pub_motor_C.publish(Float64(1))	# The velocity of motor C
+				rospy.sleep(delay_palanca)
+				pub_motor_C.publish(Float64(0))	# The velocity of motor C
+				rospy.sleep(1)
+				# gira hacia izquierda
+				pub_motor_L.publish(Float64(-1.4))	# The velocity of motor A
+			 	pub_motor_R.publish(Float64( 1.4))	# The velocity of motor B
+				rospy.sleep(2)
+				SensorData.caja=0
 		else:
-			motor_v_iz, motor_v_der = motors_velocity(SensorData.il,SensorData.dist)
-			rospy.loginfo(motor_v_iz)
-	 		rospy.loginfo(motor_v_der)
-			pub.publish(Float64(motor_v_iz))	# The velocity of motor A
-		 	pub2.publish(Float64(motor_v_der))	# The velocity of motor B
+			motor_v_L, motor_v_R = motors_velocity(SensorData.il,SensorData.dist)
+			rospy.loginfo(motor_v_L)
+	 		rospy.loginfo(motor_v_R)
+			pub_motor_L.publish(Float64(motor_v_L))	# The velocity of motor A
+		 	pub_motor_R.publish(Float64(motor_v_R))	# The velocity of motor B
 		rate.sleep()
 #-------------------------------------------------------
     # spin() simply keeps python from exiting until this node is stopped
